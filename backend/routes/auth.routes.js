79eb20fd4b11909f39eac9d40be2db69ee4d25db
Viviana Alba <vivianaalba7@gmail.com>
2024-03-11 21:26:50 -0700

    set up back end, connect MongoDB, routes

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,32 @@
+import express from "express";
+import passport from "passport";
+
+const router = express.Router();
+
+// handles /github
+router.get("/github", passport.authenticate("github", { scope: ["user:email"] }));
+
+//handles /github/callback
+router.get("/github/callback",passport.authenticate("github", { failureRedirect: process.env.CLIENT_BASE_URL + "/login" }),
+	function (req, res) {
+		res.redirect(process.env.CLIENT_BASE_URL);
+	}
+);
+
+// check if user is authenticated
+router.get("/check", (req, res) => {
+	if (req.isAuthenticated()) {
+		res.send({ user: req.user });
+	} else {
+		res.send({ user: null });
+	}
+});
+
+// handle /logout
+router.get("/logout", (req, res) => {
+	req.session.destroy((err) => {
+		res.json({ message: "Logged out" });
+	});
+});
+
+export default router;
---
+++ backend/server.js
@@ -0,0 +1,47 @@
+import express from "express";
+import dotenv from "dotenv"
+import passport from "passport";
+import session from "express-session";
+import path from "path"; // comes from node
+import "./passport/github.auth.js";
+import connectMongoDB from "./db/connectMongoDB.js";
+import authRoutes from "./routes/auth.route.js";
+
+console.log("Server is on.")
+
+// configures path to env file
+// used for environment variables - process.env
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5050;
+const __dirname = path.resolve();
+
+// determines how long user can be logged in after inactivity
+app.use(session(
+    { 
+        secret: "s0m3s0rTOf5EcR3T", 
+        resave: false, 
+        saveUninitialized: false 
+    }
+));
+
+//initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/api/auth", authRoutes);
+
+// where to find static files - web pages, audio, images
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+// send everything from the folder above and render it to our page
+// add it to DOM
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+app.listen(PORT, () => {
+	console.log(`Server started on http://localhost:${PORT}`);
+    connectMongoDB();
+});
